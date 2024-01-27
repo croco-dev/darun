aef5322b88d0769124d84c1e2e89dbc7f0ac2a9a
doda <ddark.kr@gmail.com>
2024-01-27 20:38:16 +0900

    chore: 로컬 next-font 패키지 삭제

--- libs/shared/utils-next-fonts/src/pretendard/index.ts
+++
@@ -1,1 +0,0 @@
-export { pretendardFont } from './pretendard';
--- libs/shared/utils-next-fonts/src/pretendard/pretendard.ts
+++
@@ -1,6 +0,0 @@
-import localFont from 'next/font/local';
-
-export const pretendardFont = localFont({
-  src: './PretendardVariable.woff2',
-  display: 'swap',
-});
