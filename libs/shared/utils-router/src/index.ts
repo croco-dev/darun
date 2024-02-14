329a32d5a11adf6bb37df30a657cae00e134b96b
도다 <ddark.kr@gmail.com>
2024-02-14 23:02:40 +0900

    feat: utils-router (#42)

---
+++ libs/shared/utils-router/src/index.ts
@@ -0,0 +1,4 @@
+import Link from 'next/link';
+import { usePathname, useSearchParams, useParams } from 'next/navigation';
+export { Link, useSearchParams, usePathname, useParams };
+export { useNavigate } from './libs/useNavigate';
