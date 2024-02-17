fe9c532c3f5f52e18a820b71d8e393d55e61bea7
doda <ddark.kr@gmail.com>
2024-02-17 17:05:09 +0900

    refactor: 리뷰

--- libs/frontend/src/libs/products/shells/MainHeroBanner/MainHeroBanner.tsx
+++ libs/frontend/src/libs/products/shells/MainHeroBanner/MainHeroBanner.tsx
@@ -8,47 +8,54 @@ export function MainHeroBanner() {
       <Flex position={'absolute'} top={0} bottom={0} right={-140} zIndex={20}>
         <img src={'/images/main-hero-banner.png'} />
       </Flex>
-      <ContentArea zIndex={40}>
-        <VStack py={'60px'} px={'12px'} gap={'24px'}>
-          <VStack gap={'8px'}>
-            <Text fontSize={'18px'} fontWeight={'fontWeights.medium'} letterSpacing={'-.8px'} color={'colors.dark.300'}>
-              다른 팀이 손수 비교한 서비스들을 찾고, 쓰고, 평가합니다
-            </Text>
-            <Text fontSize={'32px'} fontWeight={'fontWeights.bold'} letterSpacing={'-.8px'} color={'colors.dark.100'}>
-              <Text as={'span'} color={'colors.brown.600'}>
-                139
+      <Flex zIndex={40}>
+        <ContentArea>
+          <VStack py={'60px'} px={'12px'} gap={'24px'}>
+            <VStack gap={'8px'}>
+              <Text
+                fontSize={'18px'}
+                fontWeight={'fontWeights.medium'}
+                letterSpacing={'-.8px'}
+                color={'colors.dark.300'}
+              >
+                다른 팀이 손수 비교한 서비스들을 찾고, 쓰고, 평가합니다
               </Text>
-              개의{' '}
-              <Text as={'span'} color={'colors.brown.600'}>
-                다른
-              </Text>{' '}
-              서비스를 발견하세요
-            </Text>
+              <Text fontSize={'32px'} fontWeight={'fontWeights.bold'} letterSpacing={'-.8px'} color={'colors.dark.100'}>
+                <Text as={'span'} color={'colors.brown.600'}>
+                  139
+                </Text>
+                개의{' '}
+                <Text as={'span'} color={'colors.brown.600'}>
+                  다른
+                </Text>{' '}
+                서비스를 발견하세요
+              </Text>
+            </VStack>
+            <HStack
+              background={'#fff'}
+              flexGrow={1}
+              borderRadius={14}
+              border="1px solid rgba(0, 0, 0, 0.15)"
+              px={16}
+              py={11}
+              borderStyle="solid"
+              gap={8}
+              boxShadow="0px 2px 4px 0px rgba(0, 0, 0, 0.08)"
+            >
+              <SearchIcon size={18} />
+              <Input
+                width="100%"
+                border="none"
+                outline="none"
+                placeholder="이곳을 눌러 빠르게 검색하세요."
+                color="colors.dark.700"
+                fontSize={15}
+                letterSpacing={-0.1}
+              />
+            </HStack>
           </VStack>
-          <HStack
-            background={'#fff'}
-            flexGrow={1}
-            borderRadius={14}
-            border="1px solid rgba(0, 0, 0, 0.15)"
-            px={16}
-            py={11}
-            borderStyle="solid"
-            gap={8}
-            boxShadow="0px 2px 4px 0px rgba(0, 0, 0, 0.08)"
-          >
-            <SearchIcon size={18} />
-            <Input
-              width="100%"
-              border="none"
-              outline="none"
-              placeholder="이곳을 눌러 빠르게 검색하세요."
-              color="colors.dark.700"
-              fontSize={15}
-              letterSpacing={-0.1}
-            />
-          </HStack>
-        </VStack>
-      </ContentArea>
+        </ContentArea>
+      </Flex>
     </Flex>
   );
 }
--- libs/shared/ui-foundation/src/libs/ContentArea/ContentArea.tsx
+++ libs/shared/ui-foundation/src/libs/ContentArea/ContentArea.tsx
@@ -4,11 +4,10 @@ import { ElementType, ReactNode } from 'react';
 type ContentAreaProps = {
   children: ReactNode;
   as?: ElementType;
-  zIndex?: number;
 };
-export function ContentArea({ children, as, zIndex }: ContentAreaProps) {
+export function ContentArea({ children, as }: ContentAreaProps) {
   return (
-    <Flex as={as} w="100%" mr="auto" ml="auto" maxW={1120} pr={['1rem', '1rem', '2rem']} zIndex={zIndex}>
+    <Flex as={as} w="100%" mr="auto" ml="auto" maxW={1120} pr={['1rem', '1rem', '2rem']}>
       {children}
     </Flex>
   );
