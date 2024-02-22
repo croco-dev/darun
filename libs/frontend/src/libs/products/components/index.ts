3565af2be167f65d56cde83cd7b15f83e3d56c4c
doda <ddark.kr@gmail.com>
2024-02-23 00:19:43 +0900

    fix: export

--- libs/frontend/src/libs/products/components/index.ts
+++ libs/frontend/src/libs/products/components/index.ts
@@ -1,6 +1,6 @@
-export { ProductInformation } from "./ProductInformation";
-export { ProductSingle } from "./ProductInformation";
-export { ProductSummary } from "./ProductSummary";
-export { ProductTableOfContent } from "./ProductTableOfContent";
-export { ProductUserAction } from "./ProductUserAction";
-export { RecentProductList } from "./RecentProductList";
+export { ProductInformation } from './ProductInformation';
+export { ProductPhotos } from './ProductPhotos';
+export { ProductSummary } from './ProductSummary';
+export { ProductTableOfContent } from './ProductTableOfContent';
+export { ProductUserAction } from './ProductUserAction';
+export { RecentProductList } from './RecentProductList';
