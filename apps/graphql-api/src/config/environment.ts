4932e046be4a0241c6025425d7727241257f623d
Owen <heewon.dev@gmail.com>
2024-02-24 15:58:53 +0900

    fix: firebase key

--- apps/graphql-api/src/config/environment.ts
+++ apps/graphql-api/src/config/environment.ts
@@ -6,7 +6,6 @@ assert(process.env['DATABASE_USERNAME'], 'DATABASE_USERNAME not provided');
 assert(process.env['DATABASE_PASSWORD'], 'DATABASE_PASSWORD not provided');
 assert(process.env['FIREBASE_PROJECT_ID'], 'FIREBASE_PROJECT_ID not provided');
 assert(process.env['FIREBASE_PRIVATE_KEY'], 'FIREBASE_PRIVATE_KEY not provided');
-assert(JSON.parse(process.env['FIREBASE_PRIVATE_KEY'])['privateKey'], 'FIREBASE_PRIVATE_KEY not provided');
 assert(process.env['FIREBASE_CLIENT_EMAIL'], 'FIREBASE_CLIENT_EMAIL not provided');
 
 export const IS_LOCAL = process.env['INFRA_ENV'] === 'local';
@@ -14,5 +13,5 @@ export const DATABASE_URL = process.env['DATABASE_URL'];
 export const DATABASE_USERNAME = process.env['DATABASE_USERNAME'];
 export const DATABASE_PASSWORD = process.env['DATABASE_PASSWORD'];
 export const FIREBASE_PROJECT_ID = process.env['FIREBASE_PROJECT_ID'];
-export const FIREBASE_PRIVATE_KEY = JSON.parse(process.env['FIREBASE_PRIVATE_KEY'])['privateKey'] as string;
+export const FIREBASE_PRIVATE_KEY = process.env['FIREBASE_PRIVATE_KEY'];
 export const FIREBASE_CLIENT_EMAIL = process.env['FIREBASE_CLIENT_EMAIL'];
