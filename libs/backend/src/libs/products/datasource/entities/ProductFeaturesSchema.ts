8ef316253a1237ba6eb6ec6329d292e4d31fb762
Owen <heewon.dev@gmail.com>
2024-03-08 23:16:20 +0900

    fix: emoji

--- libs/backend/src/libs/products/datasource/entities/ProductFeaturesSchema.ts
+++ libs/backend/src/libs/products/datasource/entities/ProductFeaturesSchema.ts
@@ -6,6 +6,7 @@ export const productFeatures = pgTable('product_features', {
   id: varchar('id', { length: 26 }).primaryKey().$default(ulid),
   productId: varchar('product_id', { length: 26 }).notNull(),
   name: varchar('name', { length: 100 }).notNull(),
+  emoji: varchar('emoji', { length: 30 }).notNull(),
   summary: varchar('summary', { length: 255 }),
   createdAt: timestamp('created_at')
     .notNull()
