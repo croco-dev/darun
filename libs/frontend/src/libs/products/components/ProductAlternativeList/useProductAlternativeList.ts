c06fbeede698e8b22e2583dd9a6e92c77679dbe5
doda <ddark.kr@gmail.com>
2024-03-10 23:33:00 +0900

    feat: alternative 페이지 연동

--- libs/frontend/src/libs/products/components/ProductAlternativeList/useProductAlternativeList.ts
+++ libs/frontend/src/libs/products/components/ProductAlternativeList/useProductAlternativeList.ts
@@ -1,7 +1,39 @@
+import { gql } from '@apollo/client';
+import { useProductWithFeaturesOnProductAlternativeListSuspenseQuery } from './__generated__/useProductAlternativeList';
+
+gql`
+  query ProductWithFeaturesOnProductAlternativeList($slug: String!) {
+    productBySlug(slug: $slug) {
+      id
+      alternatives {
+        id
+        slug
+        name
+        logoUrl
+        summary
+        tags {
+          id
+          name
+        }
+        description
+        features {
+          emoji
+          id
+          name
+          summary
+        }
+      }
+    }
+  }
+`;
+
 type ProductAlternativeListProps = {
   slug: string;
 };
 
 export const useProductAlternativeList = ({ slug }: ProductAlternativeListProps) => {
-  return {};
+  const { data } = useProductWithFeaturesOnProductAlternativeListSuspenseQuery({ variables: { slug } });
+  return {
+    products: data.productBySlug?.alternatives,
+  };
 };
