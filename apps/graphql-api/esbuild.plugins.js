ef33f324e03e5e3525536b923b3c7a69073037bd
Owen <heewon.dev@gmail.com>
2024-06-03 13:39:22 +0900

    fix: backend commonjs

--- apps/graphql-api/esbuild.plugins.js
+++ apps/graphql-api/esbuild.plugins.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { esbuildDecorators } = require('@kang-heewon/esbuild-plugin-typescript-decorators');
 
 module.exports = [esbuildDecorators({})];
