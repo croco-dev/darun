feee7d53f0caa7024b74f3b077737848dc66bf9e
Owen <heewon.dev@gmail.com>
2024-06-05 00:10:12 +0900

    feat: alternative keyword

--- apps/service-web/app/products/[slug]/alternatives/page.tsx
+++ apps/service-web/app/products/[slug]/alternatives/page.tsx
@@ -26,6 +26,15 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 
   return {
     title: `${name}의 다른 서비스 - 다른(darun)`,
+    description: `${name}의 다른 서비스를 찾아보세요. 다른(darun)에서는 ${name}과 비슷한 다양한 서비스들을 비교하고, 사용자들이 평가한 서비스들을 찾아볼 수 있습니다.`,
+    keywords: [
+      `${name} 비슷한 서비스`,
+      `${name} 대안`,
+      `${name} 비슷한 사이트`,
+      `${name} 비슷한 앱`,
+      `${name} 비슷한`,
+      `${name} 말고`,
+    ],
     openGraph: {
       title: `${name}의 다른 서비스 - 다른(darun)`,
     },
