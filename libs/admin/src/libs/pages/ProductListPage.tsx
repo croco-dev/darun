1f604d99952f968d2c7e7fcd8b33dc5d7caef49a
doda <ddark.kr@gmail.com>
2024-06-22 18:57:51 +0900

    feat(admin): pageshell 추가

--- libs/admin/src/libs/pages/ProductListPage.tsx
+++ libs/admin/src/libs/pages/ProductListPage.tsx
@@ -1,20 +1,21 @@
 import { Link } from '@darun/utils-router';
-import { Button, Flex, Stack, Title } from '@mantine/core';
+import { Button } from '@mantine/core';
 import { ProductListSection } from '@products/shells';
-import { AppShell } from '@uis';
+import { AppShell, PageShell } from '@uis';
 
 export const ProductListPage = () => {
   return (
     <AppShell>
-      <Stack w="100%" p={16}>
-        <Flex direction="row" justify="space-between">
-          <Title>서비스 목록</Title>
+      <PageShell
+        title={'서비스 목록'}
+        rightSide={
           <Link href="/products/new">
             <Button>추가하기</Button>
           </Link>
-        </Flex>
+        }
+      >
         <ProductListSection />
-      </Stack>
+      </PageShell>
     </AppShell>
   );
 };
