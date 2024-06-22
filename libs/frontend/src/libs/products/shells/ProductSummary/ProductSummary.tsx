c906f4e47eeb2c6f59bbd0b76c5c5cfd58cab0bc
도다 <ddark.kr@gmail.com>
2024-06-22 23:37:33 +0900

    style(web): service-web 디자인 디테일 수정 (#135)
    * style(web): 검색 창 길이 맞춤
    
    * feat(web): 서비스 유저 액션 임시 제거 (미완)
    
    * style(web): 스타일 디테일 수정
    
    * style(web): 메인 페이지 디테일

--- libs/frontend/src/libs/products/shells/ProductSummary/ProductSummary.tsx
+++ libs/frontend/src/libs/products/shells/ProductSummary/ProductSummary.tsx
@@ -1,11 +1,10 @@
 import { HStack } from '@kuma-ui/core';
-import { ProductInformation, ProductUserAction } from '@products/components';
+import { ProductInformation } from '@products/components';
 
 type ProductSummaryProps = { slug: string };
 
 export const ProductSummary = ({ slug }: ProductSummaryProps) => (
   <HStack py="16px" justifyContent={'space-between'} alignItems={'center'}>
     <ProductInformation slug={slug} />
-    <ProductUserAction />
   </HStack>
 );
