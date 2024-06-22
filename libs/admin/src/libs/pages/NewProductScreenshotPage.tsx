2624a1346b59d2a8223da8277712ef23043720df
doda <ddark.kr@gmail.com>
2024-06-22 20:03:29 +0900

    feat(admin): 서비스 스크린샷 페이지 개선

--- libs/admin/src/libs/pages/NewProductScreenshotPage.tsx
+++ libs/admin/src/libs/pages/NewProductScreenshotPage.tsx
@@ -1,8 +1,20 @@
+'use client';
+
+import { Card, Text } from '@mantine/core';
 import { NewProductScreenshotFormSection } from '@products/shells';
-import { AppShell } from '@uis';
+import { AppShell, PageShell } from '@uis';
 
 export const NewProductScreenshotPage = ({ params: { slug } }: { params: { slug: string } }) => (
   <AppShell>
-    <NewProductScreenshotFormSection productSlug={slug} />
+    <PageShell title={'서비스에 스크린샷 추가'}>
+      <Card withBorder shadow="sm" radius="md">
+        <Card.Section withBorder inheritPadding py="xs">
+          <Text fw={500}>스크린샷</Text>
+        </Card.Section>
+        <Card.Section inheritPadding mt="sm" pb="md">
+          <NewProductScreenshotFormSection productSlug={slug} />
+        </Card.Section>
+      </Card>
+    </PageShell>
   </AppShell>
 );
