60502f4173729dfdd1f023a1a59bfab63aad238b
도다 <ddark.kr@gmail.com>
2024-08-07 17:36:40 +0900

    fix(service-web): productItem 줄바꿈 옵션 추가 (#167)

--- libs/frontend/src/libs/products/components/ProductInformation/ProductInformation.tsx
+++ libs/frontend/src/libs/products/components/ProductInformation/ProductInformation.tsx
@@ -5,5 +5,5 @@ import { ProductItem } from '@products/uis';
 import { useProductInformation } from './useProductInformation';
 
 export const ProductInformation = bind(useProductInformation, ({ name, logoUrl, summary }) => (
-  <ProductItem name={name ?? ''} logoUrl={logoUrl} summary={summary} isAlignCenter nameAs="h1" />
+  <ProductItem name={name ?? ''} logoUrl={logoUrl} summary={summary} isSummaryNoWrap={true} isAlignCenter nameAs="h1" />
 ));
