a0a792e3f71c53cf6d1575de27cd4f0099ecfa56
doda <ddark.kr@gmail.com>
2024-08-09 21:51:49 +0900

    refactor(admin-web): 쿼리 캐시 삭제가 아닌 refetch하도록 변경

--- libs/admin/src/libs/products/components/ProductListRefreshButton/useProductListRefreshButton.ts
+++ libs/admin/src/libs/products/components/ProductListRefreshButton/useProductListRefreshButton.ts
@@ -1,12 +1,11 @@
 import { useApolloClient } from '@apollo/client';
-import { useRouter } from 'next/navigation';
 
 export function useProductListRefreshButton() {
-  const { cache } = useApolloClient();
-  const { refresh: routeRefresh } = useRouter();
-  const refresh = () => {
-    cache.reset();
-    routeRefresh();
+  const apolloClient = useApolloClient();
+
+  const refresh = async () => {
+    await apolloClient.refetchQueries({ include: ['AllProductsOnProductListTable'] });
   };
+
   return { refresh };
 }
