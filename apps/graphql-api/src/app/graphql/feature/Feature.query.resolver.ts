84877301dc503a1fc19b90f63e1e349a4fac1ae0
doda <ddark.kr@gmail.com>
2024-08-12 23:53:43 +0900

    feat(graphql-api): adminFeatureById 쿼리 추가

--- apps/graphql-api/src/app/graphql/feature/Feature.query.resolver.ts
+++ apps/graphql-api/src/app/graphql/feature/Feature.query.resolver.ts
@@ -1,5 +1,6 @@
-import { GetProductFeatureScreenshots } from '@darun/backend';
-import { FieldResolver, Resolver, Root } from 'type-graphql';
+import { GetProductFeature, GetProductFeatureScreenshots } from '@darun/backend';
+import { AuthRole } from '@darun/utils-apollo-server';
+import { Arg, Authorized, FieldResolver, Query, Resolver, Root } from 'type-graphql';
 import { Service } from 'typedi';
 import { Feature } from './graphs/Feature';
 import { FeatureScreenshot } from './graphs/FeatureScreenshot';
@@ -7,10 +8,19 @@ import { FeatureScreenshot } from './graphs/FeatureScreenshot';
 @Resolver(() => Feature)
 @Service()
 export class FeatureQueryResolver {
-  constructor(private readonly getProductFeatureScreenshots: GetProductFeatureScreenshots) {}
+  constructor(
+    private readonly getProductFeatureScreenshots: GetProductFeatureScreenshots,
+    private readonly getProductFeature: GetProductFeature
+  ) {}
 
   @FieldResolver(() => [FeatureScreenshot])
   public screenshots(@Root() feature: Feature) {
     return this.getProductFeatureScreenshots.execute({ featureId: feature.id });
+  }
+
+  @Authorized([AuthRole.Admin])
+  @Query(() => Feature, { nullable: true })
+  public adminFeatureById(@Arg('id', () => String) id: string) {
+    return this.getProductFeature.execute({ id });
   }
 }
