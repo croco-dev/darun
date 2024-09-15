a4e3c9a147e1cce6adb0252d8a2e31701fe0f279
도다 <ddark.kr@gmail.com>
2024-09-15 21:41:26 +0900

    feat(service-web): og 이미지 생성 (#199)

--- libs/frontend/src/index.ts
+++ libs/frontend/src/index.ts
@@ -1,4 +1,9 @@
+// pages
 export { HomePage } from './libs/pages/HomePage';
 export { ProductDetailPage } from './libs/pages/ProductDetailPage';
 export { ProductAlternativePage } from './libs/pages/ProductAlternativePage';
 export { SearchProductPage } from './libs/pages/SearchProductPage';
+
+// opengraph
+export { OgDefaultFrame } from './libs/opengraph/components/OgDefaultFrame';
+export { useImageResponse } from './libs/opengraph/utils/useImageResponse';
