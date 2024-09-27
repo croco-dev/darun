8ed50717d21b05b822977af4975476c08932a329
doda <ddark.kr@gmail.com>
2024-09-27 21:53:38 +0900

    feat(admin): productListTable

--- libs/admin/src/libs/products/components/ProductListTable/__generated__/useProductListTable.ts
+++ libs/admin/src/libs/products/components/ProductListTable/__generated__/useProductListTable.ts
@@ -3,24 +3,37 @@ import * as Types from '@darun/provider-graphql';
 import { gql } from '@apollo/client';
 import * as Apollo from '@apollo/client';
 const defaultOptions = {} as const;
-export type AllProductsOnProductListTableQueryVariables = Types.Exact<{ [key: string]: never; }>;
+export type AllProductsOnProductListTableQueryVariables = Types.Exact<{
+  first?: Types.InputMaybe<Types.Scalars['Int']['input']>;
+  after?: Types.InputMaybe<Types.Scalars['String']['input']>;
+  last?: Types.InputMaybe<Types.Scalars['Int']['input']>;
+  before?: Types.InputMaybe<Types.Scalars['String']['input']>;
+}>;
 
 
-export type AllProductsOnProductListTableQuery = { __typename?: 'Query', allProducts: { __typename?: 'ProductConnection', edges: Array<{ __typename?: 'ProductEdge', cursor: string, node: { __typename?: 'Product', id: string, slug: string, name: string, summary: string } }> } };
+export type AllProductsOnProductListTableQuery = { __typename?: 'Query', allProducts: { __typename?: 'ProductConnection', totalCount: number, edges: Array<{ __typename?: 'ProductEdge', cursor: string, node: { __typename?: 'Product', id: string, logoUrl: string, slug: string, name: string, summary: string } }>, pageInfo: { __typename?: 'PageInfo', endCursor?: string | null, hasNextPage: boolean, startCursor?: string | null, hasPreviousPage: boolean } } };
 
 
 export const AllProductsOnProductListTableDocument = gql`
-    query AllProductsOnProductListTable {
-  allProducts(first: 50) {
+    query AllProductsOnProductListTable($first: Int, $after: String, $last: Int, $before: String) {
+  allProducts(first: $first, after: $after, last: $last, before: $before) {
     edges {
       cursor
       node {
         id
+        logoUrl
         slug
         name
         summary
       }
     }
+    pageInfo {
+      endCursor
+      hasNextPage
+      startCursor
+      hasPreviousPage
+    }
+    totalCount
   }
 }
     `;
@@ -37,6 +50,10 @@ export const AllProductsOnProductListTableDocument = gql`
  * @example
  * const { data, loading, error } = useAllProductsOnProductListTableQuery({
  *   variables: {
+ *      first: // value for 'first'
+ *      after: // value for 'after'
+ *      last: // value for 'last'
+ *      before: // value for 'before'
  *   },
  * });
  */
