4bf8599f45b548381f0a7bab0c11d8460c4f8218
도다 <ddark.kr@gmail.com>
2024-10-03 18:13:45 +0900

    feat(frontend): 스크린샷이 없으면 없다는 메시지 출력 (#220)

--- libs/frontend/src/libs/products/components/ProductPhotos/ProductPhotos.tsx
+++ libs/frontend/src/libs/products/components/ProductPhotos/ProductPhotos.tsx
@@ -1,38 +1,55 @@
 'use client';
 
 import { bind } from '@croco/utils-structure-react';
-import { css, Flex, HStack } from '@kuma-ui/core';
+import { css, Flex, HStack, Text } from '@kuma-ui/core';
 import Image from 'next/image';
 import { useProductPhotos } from './useProductPhotos';
 
-export const ProductPhotos = bind(useProductPhotos, ({ photos }) => (
-  <Flex
-    py={'8px'}
-    px={'8px'}
-    boxShadow={'0px 2px 8px 0px rgba(0, 0, 0, 0.10)'}
-    border={'1px solid rgba(0, 0, 0, 0.1)'}
-    borderRadius={'10px'}
-  >
-    {photos && (
-      <HStack gap={'8px'} width={'max-content'} overflow={'auto'}>
-        {photos.map(photo => (
-          <Image
-            key={photo.imageUrl}
-            src={photo.imageUrl}
-            alt={photo.imageAlt}
-            sizes="240px"
-            fill={true}
-            className={css`
-              border-radius: 4px;
-              border: 1px solid rgba(0, 0, 0, 0.04);
-              object-fit: contain;
-              width: auto !important;
-              height: 220px !important;
-              position: relative !important;
-            `}
-          />
-        ))}
-      </HStack>
-    )}
-  </Flex>
-));
+export const ProductPhotos = bind(useProductPhotos, ({ photos }) => {
+  if (!photos || photos.length === 0) {
+    return (
+      <Flex
+        py={'16px'}
+        px={'16px'}
+        boxShadow={'0px 2px 8px 0px rgba(0, 0, 0, 0.10)'}
+        border={'1px solid rgba(0, 0, 0, 0.1)'}
+        borderRadius={'10px'}
+      >
+        <Text fontSize={'14px'} color={'colors.dark.500'}>
+          스크린샷이 준비되지 않았어요 😢
+        </Text>
+      </Flex>
+    );
+  }
+  return (
+    <Flex
+      py={'8px'}
+      px={'8px'}
+      boxShadow={'0px 2px 8px 0px rgba(0, 0, 0, 0.10)'}
+      border={'1px solid rgba(0, 0, 0, 0.1)'}
+      borderRadius={'10px'}
+    >
+      {photos && (
+        <HStack gap={'8px'} width={'max-content'} overflow={'auto'}>
+          {photos.map(photo => (
+            <Image
+              key={photo.imageUrl}
+              src={photo.imageUrl}
+              alt={photo.imageAlt}
+              sizes="240px"
+              fill={true}
+              className={css`
+                border-radius: 4px;
+                border: 1px solid rgba(0, 0, 0, 0.04);
+                object-fit: contain;
+                width: auto !important;
+                height: 220px !important;
+                position: relative !important;
+              `}
+            />
+          ))}
+        </HStack>
+      )}
+    </Flex>
+  );
+});
