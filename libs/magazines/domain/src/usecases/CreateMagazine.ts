a2b2997e0976c3ebd5ba7fae7b9135f203d5ef42
kang-heewon <heewon.dev@gmail.com>
2026-03-16 16:52:43 +0900

    fix(types): use import type for repository interfaces in decorator parameters

--- libs/magazines/domain/src/usecases/CreateMagazine.ts
+++ libs/magazines/domain/src/usecases/CreateMagazine.ts
@@ -1,14 +1,14 @@
 import { Inject, Service } from 'typedi';
 import { Magazine } from '../entities/Magazine';
 import { magazineCreateFailed } from '../errors/magazineError';
-import type { MagazineRepository } from '../repositories/MagazineRepository';
+import { MagazineRepository } from '../repositories/MagazineRepository';
 import { MagazineRepositoryToken } from '../repositories/MagazineRepository';
 
 @Service()
 export class CreateMagazine {
   constructor(
     @Inject(MagazineRepositoryToken)
-    private readonly magazineRepository: MagazineRepository,
+    private readonly magazineRepository: MagazineRepository
   ) {}
 
   async execute({
