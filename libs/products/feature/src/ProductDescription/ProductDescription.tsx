2ba0b468d502bef19e9a685e25fb283ef1973387
kang-heewon <heewon.dev@gmail.com>
2026-03-19 02:43:46 +0900

    fix(products): use dynamic import for dompurify

--- libs/products/feature/src/ProductDescription/ProductDescription.tsx
+++ libs/products/feature/src/ProductDescription/ProductDescription.tsx
@@ -4,30 +4,27 @@ import { bind } from '@croco/utils-structure-react';
 import { useEffect, useState } from 'react';
 import { useProductDescription } from './useProductDescription';
 
-export const ProductDescription = bind(
-  useProductDescription,
-  ({ description }) => {
-    const [sanitizedDescription, setSanitizedDescription] = useState('');
+export const ProductDescription = bind(useProductDescription, ({ description }) => {
+  const [sanitizedDescription, setSanitizedDescription] = useState('');
 
-    useEffect(() => {
-      if (description) {
-        import('dompurify').then((DOMPurify) => {
-          setSanitizedDescription(DOMPurify.default.sanitize(description));
-        });
-      }
-    }, [description]);
-
-    if (!description) {
-      return <p className="text-xs text-black/60">설명이 없습니다.</p>;
+  useEffect(() => {
+    if (description) {
+      import('dompurify').then(DOMPurify => {
+        setSanitizedDescription(DOMPurify.default.sanitize(description));
+      });
     }
+  }, [description]);
 
-    return (
-      <div
-        className="whitespace-pre-wrap text-sm leading-6 text-dark-900"
-        dangerouslySetInnerHTML={{
-          __html: sanitizedDescription || description,
-        }}
-      />
-    );
-  },
-);
+  if (!description) {
+    return <p className="text-xs text-black/60">설명이 없습니다.</p>;
+  }
+
+  return (
+    <div
+      className="whitespace-pre-wrap text-sm leading-6 text-dark-900"
+      dangerouslySetInnerHTML={{
+        __html: sanitizedDescription || description,
+      }}
+    />
+  );
+});
--- libs/products/shell/src/components/ProductDescription/ProductDescription.tsx
+++ libs/products/shell/src/components/ProductDescription/ProductDescription.tsx
@@ -5,31 +5,28 @@ import { useEffect, useState } from 'react';
 
 import { useProductDescription } from './useProductDescription';
 
-export const ProductDescription = bind(
-  useProductDescription,
-  ({ description }) => {
-    const [sanitizedDescription, setSanitizedDescription] = useState('');
+export const ProductDescription = bind(useProductDescription, ({ description }) => {
+  const [sanitizedDescription, setSanitizedDescription] = useState('');
 
-    useEffect(() => {
-      if (description) {
-        import('dompurify').then((DOMPurify) => {
-          setSanitizedDescription(DOMPurify.default.sanitize(description));
-        });
-      }
-    }, [description]);
-
-    if (!description) {
-      return null;
+  useEffect(() => {
+    if (description) {
+      import('dompurify').then(DOMPurify => {
+        setSanitizedDescription(DOMPurify.default.sanitize(description));
+      });
     }
+  }, [description]);
 
-    return (
-      <div className="text-[15px] font-normal leading-[1.5] text-dark-700 [&_blockquote]:my-6 [&_blockquote]:border-l-4 [&_blockquote]:border-dark-900 [&_blockquote]:bg-dark-100 [&_blockquote]:px-4 [&_blockquote]:py-3 [&_blockquote]:text-dark-900 [&_blockquote]:not-italic [&_br]:block [&_br]:content-[''] [&_br]:mb-1 [&_hr]:my-6 [&_hr]:border-0 [&_hr]:border-t [&_hr]:border-solid [&_hr]:border-dark-100 [&_p]:my-1 [&_p.blank]:hidden">
-        <div
-          dangerouslySetInnerHTML={{
-            __html: sanitizedDescription || description,
-          }}
-        />
-      </div>
-    );
-  },
-);
+  if (!description) {
+    return null;
+  }
+
+  return (
+    <div className="text-[15px] font-normal leading-[1.5] text-dark-700 [&_blockquote]:my-6 [&_blockquote]:border-l-4 [&_blockquote]:border-dark-900 [&_blockquote]:bg-dark-100 [&_blockquote]:px-4 [&_blockquote]:py-3 [&_blockquote]:text-dark-900 [&_blockquote]:not-italic [&_br]:block [&_br]:content-[''] [&_br]:mb-1 [&_hr]:my-6 [&_hr]:border-0 [&_hr]:border-t [&_hr]:border-solid [&_hr]:border-dark-100 [&_p]:my-1 [&_p.blank]:hidden">
+      <div
+        dangerouslySetInnerHTML={{
+          __html: sanitizedDescription || description,
+        }}
+      />
+    </div>
+  );
+});
