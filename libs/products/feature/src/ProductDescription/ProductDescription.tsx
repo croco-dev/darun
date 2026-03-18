35e68d748bc89cd478c81e8ccb6b7c4b071d92a0
kang-heewon <heewon.dev@gmail.com>
2026-03-18 23:58:04 +0900

    fix(products): replace isomorphic-dompurify with dompurify
    isomorphic-dompurify uses jsdom on server side which caused ESM import
    errors on Vercel runtime. Switched to client-side dompurify with
    'use client' directive to avoid server-side DOM API dependencies.

--- libs/products/feature/src/ProductDescription/ProductDescription.tsx
+++ libs/products/feature/src/ProductDescription/ProductDescription.tsx
@@ -1,16 +1,21 @@
+'use client';
+
 import { bind } from '@croco/utils-structure-react';
-import DOMPurify from 'isomorphic-dompurify';
+import DOMPurify from 'dompurify';
 import { useProductDescription } from './useProductDescription';
 
-export const ProductDescription = bind(useProductDescription, ({ description }) => (
-  <>
-    {description ? (
-      <div
-        className="whitespace-pre-wrap text-sm leading-6 text-dark-900"
-        dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(description) }}
-      ></div>
-    ) : (
-      <p className="text-xs text-black/60">설명이 없습니다.</p>
-    )}
-  </>
-));
+export const ProductDescription = bind(
+  useProductDescription,
+  ({ description }) => (
+    <>
+      {description ? (
+        <div
+          className="whitespace-pre-wrap text-sm leading-6 text-dark-900"
+          dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(description) }}
+        ></div>
+      ) : (
+        <p className="text-xs text-black/60">설명이 없습니다.</p>
+      )}
+    </>
+  ),
+);
--- libs/products/shell/src/components/ProductDescription/ProductDescription.tsx
+++ libs/products/shell/src/components/ProductDescription/ProductDescription.tsx
@@ -1,16 +1,23 @@
+'use client';
+
 import { bind } from '@croco/utils-structure-react';
-import DOMPurify from 'isomorphic-dompurify';
+import DOMPurify from 'dompurify';
 
 import { useProductDescription } from './useProductDescription';
 
-export const ProductDescription = bind(useProductDescription, ({ description }) => {
-  if (!description) {
-    return null;
-  }
+export const ProductDescription = bind(
+  useProductDescription,
+  ({ description }) => {
+    if (!description) {
+      return null;
+    }
 
-  return (
-    <div className="text-[15px] font-normal leading-[1.5] text-dark-700 [&_blockquote]:my-6 [&_blockquote]:border-l-4 [&_blockquote]:border-dark-900 [&_blockquote]:bg-dark-100 [&_blockquote]:px-4 [&_blockquote]:py-3 [&_blockquote]:text-dark-900 [&_blockquote]:not-italic [&_br]:block [&_br]:content-[''] [&_br]:mb-1 [&_hr]:my-6 [&_hr]:border-0 [&_hr]:border-t [&_hr]:border-solid [&_hr]:border-dark-100 [&_p]:my-1 [&_p.blank]:hidden">
-      <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(description) }} />
-    </div>
-  );
-});
+    return (
+      <div className="text-[15px] font-normal leading-[1.5] text-dark-700 [&_blockquote]:my-6 [&_blockquote]:border-l-4 [&_blockquote]:border-dark-900 [&_blockquote]:bg-dark-100 [&_blockquote]:px-4 [&_blockquote]:py-3 [&_blockquote]:text-dark-900 [&_blockquote]:not-italic [&_br]:block [&_br]:content-[''] [&_br]:mb-1 [&_hr]:my-6 [&_hr]:border-0 [&_hr]:border-t [&_hr]:border-solid [&_hr]:border-dark-100 [&_p]:my-1 [&_p.blank]:hidden">
+        <div
+          dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(description) }}
+        />
+      </div>
+    );
+  },
+);
