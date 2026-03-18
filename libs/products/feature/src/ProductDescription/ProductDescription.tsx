9c8399c88ff25d98b0cd53e2ce7adea54a0f4e3d
kang-heewon <heewon.dev@gmail.com>
2026-03-19 00:56:15 +0900

    revert(products): restore bind pattern and isomorphic-dompurify
    Reverted to original implementation with bind() HOC and isomorphic-dompurify
    to fix React child rendering error. Will use pnpm override to resolve ESM
    issue with @exodus/bytes instead.

--- libs/products/feature/src/ProductDescription/ProductDescription.tsx
+++ libs/products/feature/src/ProductDescription/ProductDescription.tsx
@@ -1,23 +1,19 @@
-'use client';
-
-import DOMPurify from 'dompurify';
+import { bind } from '@croco/utils-structure-react';
+import DOMPurify from 'isomorphic-dompurify';
 import { useProductDescription } from './useProductDescription';
 
-type ProductDescriptionProps = {
-  slug: string;
-};
-
-export function ProductDescription({ slug }: ProductDescriptionProps) {
-  const { description } = useProductDescription({ slug });
-
-  if (!description) {
-    return <p className="text-xs text-black/60">설명이 없습니다.</p>;
-  }
-
-  return (
-    <div
-      className="whitespace-pre-wrap text-sm leading-6 text-dark-900"
-      dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(description) }}
-    />
-  );
-}
+export const ProductDescription = bind(
+  useProductDescription,
+  ({ description }) => (
+    <>
+      {description ? (
+        <div
+          className="whitespace-pre-wrap text-sm leading-6 text-dark-900"
+          dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(description) }}
+        ></div>
+      ) : (
+        <p className="text-xs text-black/60">설명이 없습니다.</p>
+      )}
+    </>
+  ),
+);
--- libs/products/shell/src/components/ProductDescription/ProductDescription.tsx
+++ libs/products/shell/src/components/ProductDescription/ProductDescription.tsx
@@ -17,9 +17,7 @@ export function ProductDescription({ slug }: ProductDescriptionProps) {
 
   return (
     <div className="text-[15px] font-normal leading-[1.5] text-dark-700 [&_blockquote]:my-6 [&_blockquote]:border-l-4 [&_blockquote]:border-dark-900 [&_blockquote]:bg-dark-100 [&_blockquote]:px-4 [&_blockquote]:py-3 [&_blockquote]:text-dark-900 [&_blockquote]:not-italic [&_br]:block [&_br]:content-[''] [&_br]:mb-1 [&_hr]:my-6 [&_hr]:border-0 [&_hr]:border-t [&_hr]:border-solid [&_hr]:border-dark-100 [&_p]:my-1 [&_p.blank]:hidden">
-      <div
-        dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(description) }}
-      />
+      <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(description) }} />
     </div>
   );
 }
