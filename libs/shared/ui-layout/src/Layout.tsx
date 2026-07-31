0aa9cd12764c1300540e5f6ac228d0a4205f2bab
kang-heewon <heewon.dev@gmail.com>
2026-07-31 12:40:08 +0900

    Layout에서 건너뛰기 링크 제거
    사용하지 않는 기능이라 제거한다. 링크가 유일한 소비자였던
    id="main-content" 래퍼 div도 함께 걷어내고, 감싸기용으로만 남던
    fragment를 정리했다.
    
    ProductCard와 스켈레톤 파일의 파일 끝 개행은 포매터가 채운 결과다.

--- libs/shared/ui-layout/src/Layout.tsx
+++ libs/shared/ui-layout/src/Layout.tsx
@@ -5,18 +5,10 @@ import { Header } from './libs/Header';
 
 export function Layout({ children }: { children: ReactNode }) {
   return (
-    <>
-      <a
-        href="#main-content"
-        className="sr-only focus:not-sr-only focus:absolute focus:z-50 focus:p-4 focus:bg-white focus:text-dark-900"
-      >
-        본문으로 건너뛰기
-      </a>
-      <div className="flex flex-col">
-        <Header />
-        <div id="main-content">{children}</div>
-        <Footer />
-      </div>
-    </>
+    <div className="flex flex-col">
+      <Header />
+      {children}
+      <Footer />
+    </div>
   );
 }
